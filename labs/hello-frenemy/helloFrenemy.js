45e8534060bfef35b4cf5f90618ce6ab869b8668
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-02-08 17:05:07 -0500

    hello frenemy done

---
+++ labs/hello-frenemy/helloFrenemy.js
@@ -0,0 +1,11 @@
+function greeter(name) {
+    if (name == 'darth vader' || name == 'lex luthor') {
+        console.log("Go Away!")
+    } else {
+        console.log("Hello, ", name);
+    }
+}
+
+greeter("Luke Skywalker");
+greeter("darth vader");
+greeter("lex luthor");
