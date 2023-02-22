373c5f5cd881d4cb855d21bb2440677e1607ac3b
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-02-21 20:33:18 -0500

    array destructuring, objects, object methods

--- lecture/unit_3/arrayDestructuring.js
+++ lecture/unit_3/arrayDestructuring.js
@@ -97,3 +97,78 @@ persons.push({
 
 console.log("Persons Array: ", persons); // Includes Anna object in array
   console.log("Copied Persons Array: ", copiedPersons); // Copy does not include Anna. Created before Anna was added.
+
+//* Variables within Memory
+// First assignment/original assignment
+let x = 10;
+let y = "abc";
+let z = null;
+
+// Reassign to another variable
+let a = x;
+console.log(a); // 10
+let b = y;
+console.log(b); // abc
+
+// Reassign a and b
+a = 5;
+b = "def";
+console.log(x, y, a, b) // 10, abc, 5, def
+
+// --------------------------------------
+
+let arr = [];
+arr.push(1);
+console.log(`arr: ${arr}`);
+
+let reference = [1];
+let refCopy = reference;
+console.log(`refCopy: ${refCopy}`);
+
+console.log(reference, refCopy);
+
+// reference = []
+reference.push(2); // both reference and refCopy will have the 2 added
+console.log(reference, refCopy); // [1, 2] [1, 2]
+
+refCopy.push(3);
+console.log(reference, refCopy);
+
+
+//? Rest Syntax
+
+/*
+
+- Looks exactly like spread syntax: ...
+- Spread -> "expands" an array
+- Rest -> "condenses" an array
+
+
+We can use the spread operator syntax, referred to as 'rest' syntax when working with array destructuring, to package up the REST of the values in the referenced array, and return them as their own array.
+
+*/
+
+const fullNameAgain = [
+  "Jane",
+  "Doe",
+  "Mrs.",
+  {
+    month: 03,
+    date: 22,
+    year: 1973,
+  },
+  2,
+  "test",
+  "4",
+  true,
+  false,
+];
+
+let janesName = fullName[0];
+// console.log(janesName);
+
+// Using both array destructuring and rest:
+let [newFirstName, newLastName, , ...otherInfo] = fullNameAgain;
+console.log(`newFirstName: ${newFirstName}`);
+console.log(`newLastName: ${newLastName}`);
+console.log(`...otherInfo: ${otherInfo}`);
